8dd292343bb90a8aa60ea3c7d02ff77b8e1e0ed3
Cartitza <acarstian@gmail.com>
2025-02-18 16:33:36 +0200

    Image displayed + buttons for edit options

--- Src/Interface/app.py
+++ Src/Interface/app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    image = None
     if request.method == "POST":
         image = request.files['image']
+        image.save('./static/images/img.jpg')
 
-    return render_template('index.html')
+    return render_template('index.html', image=image)
 
 if __name__ == '__main__':
     app.run(debug=True)
